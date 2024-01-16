6d59b15792442d0ed0fa0606ecdf19e234d7b297
Kiru <jmgdaw8@gmail.com>
2024-01-16 09:09:29 +0100

    commit jesus original

---
+++ inventariogit/src/main/java/com/myproyecto/inventariogit/Inventariogit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproyecto.inventariogit;
+
+/**
+ *
+ * @author Kiru
+ */
+public class Inventariogit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("no viva murcia");
+    }
+}
